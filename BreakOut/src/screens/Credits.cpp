3f7a8f732478a069100422afdc92c3478cc33446
Felix Godziela <joemano.elcooll@gmail.com>
2020-09-22 09:35:30 -0300

    Added credits screen

---
+++ BreakOut/src/screens/Credits.cpp
@@ -0,0 +1,38 @@
+#include "Credits.h"
+
+namespace Godziela
+{
+	short time = 12000;
+	short aux = 0;
+	short doub = 2;
+
+	void draw_credits()
+	{
+		short offside = third_screen_height;
+
+
+		DrawText("Programming: Felix Godziela", fourteenth_screen_width, offside, font_size, RED);
+
+		offside += font_size * doub;
+
+		DrawText("Graphics: Felix Godziela - photoshop", fourteenth_screen_width, offside, font_size, RED);
+
+		offside += font_size * doub;
+
+		DrawText("Music: Cheetamen - Action 52", fourteenth_screen_width, offside, font_size, RED);
+
+		offside += font_size * doub;
+
+		DrawText("BackGround Image: https://tipsmake.com/100-beautiful-background-png", fourteenth_screen_width, offside, font_size, RED);
+
+		//
+
+		aux++;
+		if (aux >= time)
+		{
+			game_state = MENU;
+			aux = 0;
+		}
+
+	}
+}
---
+++ BreakOut/src/screens/Credits.h
@@ -0,0 +1,13 @@
+#ifndef CREDITS_H
+#define CREDITS_H
+
+#include "manager.h"
+
+namespace Godziela
+{
+	extern bool visible_credits;
+	void draw_credits();
+}
+
+
+#endif
